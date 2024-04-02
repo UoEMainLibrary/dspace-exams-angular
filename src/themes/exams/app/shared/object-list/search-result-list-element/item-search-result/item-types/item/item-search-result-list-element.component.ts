65acf2d0b5d2e2bb98243e3a8a03300f8c3073c7
Ben Parkes <benjamin.b.parkes@googlemail.com>
2024-04-02 16:19:12 +0100

    download paper button 02042024

--- src/themes/exams/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.ts
+++ src/themes/exams/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import {
   listableObjectComponent
 } from '../../../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
@@ -11,6 +11,20 @@ import {
 } from '../../../../../../../../../app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component';
 import { Context } from '../../../../../../../../../app/core/shared/context.model';
 
+import { BitstreamDataService } from '../../../../../../../../../app/core/data/bitstream-data.service';
+import { Bitstream } from '../../../../../../../../../app/core/shared/bitstream.model';
+import { Item } from '../../../../../../../../../app/core/shared/item.model';
+import { Observable } from 'rxjs';
+import { PaginatedList } from '../../../../../../../../../app/core/data/paginated-list.model';
+import { RemoteData } from '../../../../../../../../../app/core/data/remote-data';
+import { PaginationComponentOptions } from '../../../../../../../../../app/shared/pagination/pagination-component-options.model';
+import { PaginationService } from '../../../../../../../../../app/core/pagination/pagination.service';
+import { DSONameService } from '../../../../../../../../../app/core/breadcrumbs/dso-name.service';
+import { AppConfig, APP_CONFIG } from 'src/config/app-config.interface';
+import { NotificationsService } from 'src/app/shared/notifications/notifications.service';
+import { TranslateService } from '@ngx-translate/core';
+
+
 @listableObjectComponent('PublicationSearchResult', ViewMode.ListElement, Context.Any, 'custom')
 @listableObjectComponent(ItemSearchResult, ViewMode.ListElement, Context.Any, 'custom')
 @Component({
@@ -21,4 +35,50 @@ import { Context } from '../../../../../../../../../app/core/shared/context.mode
   // templateUrl: '../../../../../../../../../app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.html',
 })
 export class ItemSearchResultListElementComponent extends BaseComponent {
+
+  @Input() item: Item;
+
+  originals$: Observable<RemoteData<PaginatedList<Bitstream>>>;
+
+  originalOptions = Object.assign(new PaginationComponentOptions(), {
+    id: 'obo',
+    currentPage: 1,
+    pageSize: this.appConfig.item.bitstream.pageSize
+  });
+
+  constructor(
+    bitstreamDataService: BitstreamDataService,
+    protected notificationsService: NotificationsService,
+    protected translateService: TranslateService,
+    protected paginationService: PaginationService,
+    public dsoNameService: DSONameService,
+    @Inject(APP_CONFIG) protected appConfig: AppConfig
+  ) {
+    super(bitstreamDataService, notificationsService, translateService, dsoNameService, appConfig);
+  }
+
+  ngOnInit(): void {
+    this.initialize();
+  }
+
+  initialize(): void {
+    this.originals$ = this.paginationService.getCurrentPagination(this.originalOptions.id, this.originalOptions).pipe(
+      switchMap((options: PaginationComponentOptions) => this.bitstreamDataService.findAllByItemAndBundleName(
+        this.item,
+        'ORIGINAL',
+        {elementsPerPage: options.pageSize, currentPage: options.currentPage},
+        true,
+        true,
+        followLink('format'),
+        followLink('thumbnail'),
+      )),
+      tap((rd: RemoteData<PaginatedList<Bitstream>>) => {
+          if (hasValue(rd.errorMessage)) {
+            this.notificationsService.error(this.translateService.get('file-section.error.header'), `${rd.statusCode} ${rd.errorMessage}`);
+          }
+        }
+      )
+    );
+  }
+
 }
